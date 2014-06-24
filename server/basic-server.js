f9567a872e4a65da191f90876313d65383ef92a3
BaBuDe <caly.moss@gmail.com>
2014-06-23 22:54:02 -0700

    Working on specs...

--- server/basic-server.js
+++ server/basic-server.js
@@ -20,7 +20,7 @@ var ip = "127.0.0.1";
 we could have called it anything (myServer, blahblah, etc.). The function we pass it (handleRequest)
 will, unsurprisingly, handle all incoming requests. (ps: 'handleRequest' is in the 'request-handler' file).
 Lastly, we tell the server we made to listen on the given port and IP. */
-var request = requestHandler.handleRequest;
+var request = requestHandler.handler;
 var server = http.createServer(request);
 //option2
 // var server = http.createServer(requestHandler);
--- server/request-handler.js
+++ server/request-handler.js
@@ -1,23 +1,26 @@
 var messages = [];
+var url = {};
 
-module.exports.handleRequest = function(request, response) {
+module.exports.handler = function(request, response) {
   var headers = defaultCorsHeaders;
   var statusCode = 200;
 
   headers['Content-Type'] = 'application/json';
 
-  response.end();
   /* .writeHead() tells our server what HTTP status code to send back */
-  response.writeHead(statusCode, headers);
 //option2
 // module.exports = function handleRequest(request,response) {
 
   console.log("Serving request type " + request.method + " for url " + request.url);
 
   if (request.method === 'GET') {
+    //return 200 on success (if within url object)
+    //return 404 on failure (if not in url object)
     response.writeHead(statusCode, headers);
     response.end(JSON.stringify({results: messages}));
   } else if (request.method === 'POST') {
+    //parse request.url to see if url exists within url object
+    //if it does not exist, add it to the url object (201)
       var data = '';
     request.on('data', function(chunk){
       data += chunk;
@@ -25,9 +28,12 @@ module.exports.handleRequest = function(request, response) {
     request.on('end', function(){
       var message = JSON.parse(data);
       messages.push(message);
-      response.end(JSON.stringify(message));
+      // response.end(JSON.stringify(message));
     });
+    console.log(messages);
   }
+  response.writeHead(statusCode, headers);
+  response.end();
 };
 
 
