87b7db50ec5a803d70db8642650bb32c9b60235d
BaBuDe <caly.moss@gmail.com>
2014-06-24 11:41:17 -0700

    Adding status codes for different requests.

--- server/basic-server.js
+++ server/basic-server.js
@@ -14,16 +14,17 @@ var port = 3000;
  * special address that always refers to localhost. */
 var ip = "127.0.0.1";
 
+//http://localhost:3000/classes/messages
 //http://127.0.0.1:3000/classes/messages
 
 /* We use node's http module to create a server. Note, we called it 'server', but
 we could have called it anything (myServer, blahblah, etc.). The function we pass it (handleRequest)
-will, unsurprisingly, handle all incoming requests. (ps: 'handleRequest' is in the 'request-handler' file).
+will, unsurprisingly, handle all incoming requests. (ps: 'handler' is in the 'request-handler' file).
 Lastly, we tell the server we made to listen on the given port and IP. */
 var request = requestHandler.handler;
 var server = http.createServer(request);
 //option2
-// var server = http.createServer(requestHandler);
+//var server = http.createServer(requestHandler.handler);
 
 
 console.log("Listening on http://" + ip + ":" + port);
--- server/request-handler.js
+++ server/request-handler.js
@@ -5,22 +5,23 @@ module.exports.handler = function(request, response) {
   var headers = defaultCorsHeaders;
   var statusCode = 200;
 
+//option2
+// module.exports = function handleRequest(request,response) {
+
+//???
   headers['Content-Type'] = 'application/json';
 
   /* .writeHead() tells our server what HTTP status code to send back */
-//option2
-// module.exports = function handleRequest(request,response) {
 
   console.log("Serving request type " + request.method + " for url " + request.url);
 
   if (request.method === 'GET') {
+
     //return 200 on success (if within url object)
     //return 404 on failure (if not in url object)
     response.writeHead(statusCode, headers);
     response.end(JSON.stringify({results: messages}));
   } else if (request.method === 'POST') {
-    //parse request.url to see if url exists within url object
-    //if it does not exist, add it to the url object (201)
       var data = '';
     request.on('data', function(chunk){
       data += chunk;
@@ -28,10 +29,16 @@ module.exports.handler = function(request, response) {
     request.on('end', function(){
       var message = JSON.parse(data);
       messages.push(message);
-      // response.end(JSON.stringify(message));
+      //response.end(JSON.stringify(message));
+      //if URL does not exist
+      if (!url[request.url]) {
+        //add incoming request URL to storage object
+        url[request.url] = true;
+        statusCode = 201;
+        console.log(statusCode);
+      }
     });
-    console.log(messages);
-  }
+  };
   response.writeHead(statusCode, headers);
   response.end();
 };
